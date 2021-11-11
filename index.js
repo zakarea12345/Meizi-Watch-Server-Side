3ac70c4b7354f13a52ef9d04f5df1f3ebb71f3df
zakarea12345 <saiakzakarea7@gmail.com>
2021-11-11 16:14:51 +0600

    mongodb uri is working

--- index.js
+++ index.js
@@ -9,11 +9,8 @@ const port = process.env.PORT || 5000;
 
 //https://dialz-theme.myshopify.com/
 
-
 //mongodb uri and connection
-const uri = "mongodb+srv://<username>:<password>@cluster0.i2xld.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-
-
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.i2xld.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
