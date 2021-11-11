01dc8c130d0b1696fabea0124d86a19a2e14112f
zakarea12345 <saiakzakarea7@gmail.com>
2021-11-11 17:15:09 +0600

    products get and insert successfully

--- index.js
+++ index.js
@@ -9,25 +9,35 @@ const port = process.env.PORT || 5000;
 
 //https://dialz-theme.myshopify.com/
 
+// middleware
+app.use(cors());
+app.use(express.json());
+
 //mongodb uri and connection
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.i2xld.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
-// middleware
-app.use(cors());
-app.use(express.json());
+async function run(){
+    try{
+        await client.connect();
+        const database = client.db('watchStoreInformation');
+        const productsCollection =  database.collection('products');
+        //get products
+        app.get('/products', async(req,res)=>{
+            const cursor = productsCollection.find({});
+            const products =  await cursor.toArray();
+            res.send(products)
+        });
+    }
+    finally{
+        //await client.close();
+    }
+}
 
 
-
-
-
-
-
-
-
-
-
+//call the async function
+run().catch(console.dir)
 
 //to check the side is running.
 app.get('/', (req, res) => {
