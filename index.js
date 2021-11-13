605384c54f421f3674c28c92ad05a97efe3440fd
zakarea12345 <saiakzakarea7@gmail.com>
2021-11-13 21:26:45 +0600

    done

--- index.js
+++ index.js
@@ -109,15 +109,16 @@ async function run(){
         const user =  await cursor.toArray();
         res.send(user)
       });
-     app.get('/users/:email', async (req, res) => {
-     const query = { email: email };
-     const user = await usersCollection.findOne(query);
-     let isAdmin = false;
-      if (user?.role === 'admin') {
-      isAdmin = true;
-     }
-     res.json({ admin: isAdmin });
-    });
+      app.get('/users/:email', async (req, res) => {
+        const email = req.params.email;
+        const query = { email: email };
+        const user = await usersCollection.findOne(query);
+        let isAdmin = false;
+        if (user?.role === 'admin') {
+            isAdmin = true;
+        }
+        res.json({ admin: isAdmin });
+    })
      app.post('/users', async (req, res) => {
   const user = req.body;
   const result = await usersCollection.insertOne(user);
