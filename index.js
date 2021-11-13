392862380edeed229056f4dfec8501785a969350
zakarea12345 <saiakzakarea7@gmail.com>
2021-11-13 18:02:03 +0600

    server side completed

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 
-//https://dialz-theme.myshopify.com/
-
 // middleware
 app.use(cors());
 app.use(express.json());
@@ -25,12 +23,26 @@ async function run(){
         const productsCollection =  database.collection('products');
         const orderCollection = database.collection('orders');
         const reviewCollection = database.collection('reviews');
+        const usersCollection = database.collection('users')
         //get products
         app.get('/products', async(req,res)=>{
             const cursor = productsCollection.find({});
             const products =  await cursor.toArray();
             res.send(products)
         });
+         // POST API For New Products
+         app.post('/products', async (req, res) => {
+          const newProduct = req.body;
+          const result = await productsCollection.insertOne(newProduct);
+          res.json(result);
+      })
+        //  Delete products
+      app.delete('/products/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const result = await productsCollection.deleteOne(query);
+        res.json(result);
+      });
         //get orders
         app.get('/orders', async(req,res)=>{
             const cursor = orderCollection.find({});
@@ -60,18 +72,73 @@ async function run(){
         console.log('deleting user with id', id);
         res.json(result);
       });
+      // PUT API FOR UPDATE
+      app.put('/orders/:id', async (req, res) => {
+        const id = req.params.id;
+        console.log(id)
+        const updatedOrder = req.body;
+        console.log(updatedOrder)
+        const filter = { _id: ObjectId(id) };
+        const options = { upsert: true };
+       const updateDoc = {
+            $set: {
+                orderStatus: 'Approved',
+            }
+        }
+        const result = await orderCollection.updateOne(filter, updateDoc, options);
+
+        console.log('Update Hitted ', id);
+        res.json(result);
+
+     });
        //review orders
        app.get('/reviews', async(req,res)=>{
         const cursor = reviewCollection.find({});
         const reviews =  await cursor.toArray();
         res.send(reviews)
-    });
+     });
       //post reviews
       app.post('/reviews', async (req, res) => {
         const review = req.body;
         const result = await reviewCollection.insertOne(review);
         res.json(result);
- });
+     });
+      //get users
+      app.get('/users', async(req,res)=>{
+        const cursor = usersCollection.find({});
+        const user =  await cursor.toArray();
+        res.send(user)
+      });
+     app.get('/users/:email', async (req, res) => {
+     const query = { email: email };
+     const user = await usersCollection.findOne(query);
+     let isAdmin = false;
+      if (user?.role === 'admin') {
+      isAdmin = true;
+     }
+     res.json({ admin: isAdmin });
+    });
+     app.post('/users', async (req, res) => {
+  const user = req.body;
+  const result = await usersCollection.insertOne(user);
+  console.log(result);
+  res.json(result);
+     });
+     app.put('/users', async (req, res) => {
+     const user = req.body;
+     const filter = { email: user.email };
+     const options = { upsert: true };
+     const updateDoc = { $set: user };
+     const result = await usersCollection.updateOne(filter, updateDoc, options);
+     res.json(result);
+     });
+     app.put('/users/admin', async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email };
+      const updateDoc = { $set: { role: 'admin' } };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.json(result); 
+     })
     }
     finally{
         //await client.close();
